6989444b817198cba0469e01cb71bd43a9a9330c
michaelkhanta <michael.khanta33@smk.belajar.id>
2024-09-25 09:06:24 +0800

    Update proses_like.php

--- config/proses_like.php
+++ config/proses_like.php
@@ -11,7 +11,7 @@ if (mysqli_num_rows($ceksuka) == 1) {
 		$likeid = $row['likeid'];
 		$query = mysqli_query($koneksi, "DELETE FROM likefoto WHERE likeid='$likeid'");
 		echo "<script>
-		location.href='../admin/home.php';
+		location.href='../admin/index.php';
 		</script>";
 	}
 }else{
@@ -19,7 +19,7 @@ if (mysqli_num_rows($ceksuka) == 1) {
 	$query = mysqli_query($koneksi,"INSERT INTO likefoto VALUES('','$fotoid','$userid','$tanggallike')");
 
 	echo "<script>
-	location.href='../admin/home.php';
+	location.href='../admin/index.php';
 	</script>"; 
 }
 
