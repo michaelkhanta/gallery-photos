cf9bb0e1898acc0f425da7a419e01fa1d2230131
michaelkhanta <michael.khanta33@smk.belajar.id>
2024-09-25 09:51:02 +0800

    Update aksi_album.php

--- config/aksi_album.php
+++ config/aksi_album.php
@@ -1,46 +1,45 @@
-<?php 
+<?php
 session_start();
-include 'koneksi.php';
+include "koneksi.php";
 
 if (isset($_POST['tambah'])) {
-	$nama_album = $_POST['namaalbum'];
-	$deskripsi = $_POST['deskripsi'];
-	$tanggal = date('Y-m-d');
-	$user_id = $_SESSION['userid'];
+    $namaalbum = $_POST['namaalbum'];
+    $deskripsi = $_POST['deskripsi'];
+    $tanggal = date('Y-m-d');
+    $userid = $_SESSION['userid'];
 
-	$sql = mysqli_query($koneksi, "INSERT INTO album VALUES('','$nama_album','$deskripsi','$tanggal','$user_id')");
+    $sql = mysqli_query($koneksi, "INSERT INTO album VALUES('','$namaalbum','$deskripsi','$tanggal','$userid')");
 
-	echo "<script>
-	alert('Data Berhasil Disimpan!');
-	location.href='../admin/album.php';
-	</script>";
+    echo "<script>
+    alert('Data berhasil disimpan!'); 
+    location.href='../admin/album.php';
+    </script>";
 }
 
-
 if (isset($_POST['edit'])) {
-	$album_id = $_POST['albumid'];
-	$nama_album = $_POST['namaalbum'];
-	$deskripsi = $_POST['deskripsi'];
-	$tanggal = date('Y-m-d');
-	$user_id = $_SESSION['userid'];
+    $albumid = $_POST['albumid'];
+    $namaalbum = $_POST['namaalbum'];
+    $deskripsi = $_POST['deskripsi'];
+    $tanggal = date('Y-m-d');
+    $userid = $_SESSION['userid'];
 
-	$sql = mysqli_query($koneksi, "UPDATE album SET nama_album='$nama_album', deskripsi='$deskripsi', tanggal_buat='$tanggal' WHERE album_id='$album_id'");
+    $sql = mysqli_query($koneksi,"UPDATE album SET namaalbum='$namaalbum', deskripsi='$deskripsi', tanggalbuat='$tanggal' WHERE albumid='$albumid'");
 
-	echo "<script>
-	alert('Data Berhasil Diperbarui!');
-	location.href='../admin/album.php';
-	</script>";
+    echo "<script>
+    alert('Data berhasil diperbarui!');
+    location.href='../admin/album.php';
+    </script>";
 }
 
 if (isset($_POST['hapus'])) {
-	$album_id = $_POST['albumid'];
+    $albumid = $_POST['albumid'];
 
-	$sql = mysqli_query($koneksi, "DELETE FROM album WHERE $album_id='$album_id'");
+    $sql = mysqli_query($koneksi, "DELETE FROM album WHERE albumid='$albumid'");
 
-	echo "<script>
-	alert('Data Berhasil Dihapus!');
-	location.href='../admin/album.php';
-	</script>";
+    echo "<script>
+    alert('Data berhasil dihapus!');
+    location.href='../admin/album.php';
+    </script>";
 }
 
 ?>
